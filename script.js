117b4f34d5baca87edfedfb2a3d8c916facb6a5f
mohammed sirjudeen <mohammedsira18@gamil.com>
2024-05-13 20:40:11 +0530

    DOM DEMO

---
+++ script.js
@@ -0,0 +1,14 @@
+function foo() {
+    
+    var res = document.getElementById("firstname").value;
+    console.log(res);
+
+    var res = document.getElementById("middlename").value;
+    console.log(res);
+
+    var res = document.getElementById("lastname").value;
+    console.log(res);
+
+    var res = document.getElementById("email").value;
+    console.log(res);
+}
